9cd1d855d09c249ef68bcc78f29928075a13de99
Karan M <karanunique36@gmail.com>
2025-10-17 22:44:13 +0530

    updated the resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -70,7 +70,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="/projects/Updated_Resume.pdf"
+                href="/projects/Karan_Resume.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
